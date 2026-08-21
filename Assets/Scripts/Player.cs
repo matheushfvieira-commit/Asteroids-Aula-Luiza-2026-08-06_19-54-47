f76c4536ee98ff372109682cebaa638a3a64c000
matheushfvieira-commit <matheus_hf_vieira@estudante.sesisenai.org.br>
2026-08-20 21:49:54 -0300

    Tentei colocar mov no beta da nave

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,16 +1,48 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
+
 
 public class Players : MonoBehaviour
 {
-    public float Speed;
+    public Animator animator;
+
+    public float Thurst;
+    public Rigidbody2D rb; 
     void Start()
     {
-        
+        animator  = GetComponent<Animator>();
+    }
+    private void HandleMovment()
+    {
+        if (Mouse.current.leftButton.isPressed)
+        {
+            Vector3 mousePos = Camera.current.position.value;
+            animator.SetBool("isMoving", true);
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Mouse.current.position.value);
+            Vector2  direction = (mousePos - transform.position);
+        }
+        else if (animator.GetBool("isMoving"))
+        {
+            animator.SetBool("isMoving" = false);
+        }  
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        HandleMovment();
+       //Mouse.current.leftButton.isPressed
+       
+       //Mouse.current.position.value
     }
 }
+
+
+//Mouse.current.leftButton.isPressed
+
+
+//Mouse.current.position.value
+
+//Camera.main.ScreenToWorldPoint(vector3 position)
+
+//RayCast estudar pra fazer inimigos no nosso jogo de terror
